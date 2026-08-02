95575217899c1ace289ac61095aa0b19b3463b99
hayas-97 <hayasjoyshan97@gmail.com>
2026-08-02 05:52:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+// ===============================
+// Satellite Tracker Pro
+// ===============================
 
+// Create the map
+const map = L.map("map").setView([20, 0], 3);
+
+// OpenStreetMap tiles
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "&copy; OpenStreetMap contributors"
+}).addTo(map);
+
+// Global variables
+let satellites = [];
+let satelliteMarkers = [];
+let userMarker = null;
+let selectedSatellite = null;
